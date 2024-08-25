e9536e4e274b087a8d658d0de6dddf066db4cb25
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-25 19:57:21 +0500

    add workspace variable

--- scripts/unit_test/phpunit_tests.sh
+++ scripts/unit_test/phpunit_tests.sh
@@ -4,6 +4,15 @@ echo "**********************"
 echo "Testing Laravel Image "
 echo "**********************"
 
-docker run --rm -ti -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@"
+
+
+WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/webapp_cicd
+docker run --rm  -v $WORKSPACE/jobportal:/app -v -w /app hikmatmarwat/job_portal:1.6 "$@"
+
+
+
+#docker run --rm -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@" vendor/bin/phpunit --configuration phpunit.xml
+
+#docker run --rm -ti -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@"
 
 
