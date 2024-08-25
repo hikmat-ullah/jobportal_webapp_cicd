560b67b4f8e6f7eec755b894bcb409b43e362a10
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-25 20:11:13 +0500

    removed the ti flag from the testing script

--- scripts/unit_test/phpunit_tests.sh
+++ scripts/unit_test/phpunit_tests.sh
@@ -5,14 +5,4 @@ echo "Testing Laravel Image "
 echo "**********************"
 
 
-
-WORKSPACE=/home/admin/jenkins-data/jenkins_home/workspace/webapp_cicd
-docker run --rm  -v $WORKSPACE/jobportal:/app -v -w /app hikmatmarwat/job_portal:1.6 "$@"
-
-
-
-#docker run --rm -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@" vendor/bin/phpunit --configuration phpunit.xml
-
-#docker run --rm -ti -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@"
-
-
+docker run --rm -v jobportal:/app hikmatmarwat/job_portal:1.6 "$@"
