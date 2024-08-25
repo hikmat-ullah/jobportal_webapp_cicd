50faad288b4cf6e7b65789f2afc40848f2306a44
hikmat-ullah <hikmatmarwat644@gmail.com>
2024-08-25 19:22:51 +0500

    fixed variable value in testing script

--- scripts/unit_test/phpunit_tests.sh
+++ scripts/unit_test/phpunit_tests.sh
@@ -4,6 +4,7 @@ echo "**********************"
 echo "Testing Laravel Image "
 echo "**********************"
 #unit_testing=vendor/bin/phpunit --configuration phpunit.xml
+export BUILD_TAG=$(sed -n '2p' /tmp/.creds)
 
 docker run --rm -ti -v jobportal-web-app-master:/app hikmatmarwat/job_portal:$BUILD_TAG vendor/bin/phpunit --configuration phpunit.xml
 
